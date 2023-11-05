5e71728a78bc3503d0dc92b9f55d1c99dbf27456
Arda Sevinç <ardasevinc.technical@gmail.com>
2023-11-06 00:48:57 +0300

    feat: template ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-   <div>
-    <p>Hello World</p>
-   </div>
-  )
+    <div className='h-screen bg-slate-900 text-white/90 flex flex-col justify-center items-center'>
+      <p className='text-4xl'>Hello World</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
